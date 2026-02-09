20fb6a46748ccdf8bdbfad87519be055eeda45e6
Nwachukwu Wisdom <wisdom.nwachukwu@tecnotree.com>
2026-02-09 17:35:26 +0100

    Added assigment

--- week-02-python-basics/main.py
+++ week-02-python-basics/main.py
@@ -144,15 +144,3 @@ dict_1 = {
 }
 print("Dictionary with duplicate keys: ", dict_1)
 
-"""
-Assignment:
-- Create a list of your favorite movies, including duplicates.
-- Create a set of your favorite movies, including duplicates.
-- Create a tuple of your favorite movies, including duplicates.
-- Create a dictionary with movie titles as keys and their release years as values. Include at least
-    5 movies.
-- List 3 characteristics of each data structure (list, set, tuple, dictionary).
-- what is the similarity and difference between list and tuple?
-- How many data types are there in Python? List them and give an example of each.
-- How many data structures are there in Python? List them and give an example of each.
-"""
